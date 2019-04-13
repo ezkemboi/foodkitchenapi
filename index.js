3c6fdf268fbc5c13a2dda03191171e5525d21d12
me-x-mi <developerezrqn@gmail.com>
2019-04-13 08:38:46 +0300

    add endpoints skeleton

--- index.js
+++ index.js
@@ -21,7 +21,81 @@ app.get('/', (req, res) => {
     res.status(200).send({
         success: true,
         message: "Welcome to Gennas Kitchen"
-    })
+    });
+});
+
+// Sign up to the system
+app.post('/register', (req, res) => {
+    res.status(200).send({
+        success: true,
+        message: "Product added successfully",
+    });
+});
+
+// Login
+app.post('/login', (req, res) => {
+    res.status(200).send({
+        success: true,
+        message: "Product added successfully",
+    });
+});
+
+// Get all items in store
+app.get('/products', (req, res) => {
+    const items = [
+        {
+            id: "00292999jghsss",
+            name: "Prawn",
+            price: 7.00
+        },
+        {
+            id: "00297777hgddd0088hsss",
+            name: "Crab Platter",
+            price: 15.00
+        },
+        {
+            id: "0po078888hyyyoo",
+            name: "Fries",
+            price: 1.50
+        }
+    ]
+    res.status(200).send({
+        success: true,
+        message: "Products retrieved successfully",
+        data: items
+    });
+});
+
+// Add food to the list of products
+app.post('/products', (req, res) => {
+    res.status(200).send({
+        success: true,
+        message: "Product added successfully",
+    });
+});
+
+// Get a single item
+app.get('/products/:id', (req, res) => {
+    res.status(200).send({
+        success: true,
+        message: "Product retrieved successfully",
+    });
+});
+
+// Update a single item
+app.put('/products/:id', (req, res) => {
+    res.status(200).send({
+        success: true,
+        message: "Product updated successfully",
+    });
+});
+
+// Delete a single item
+app.delete('/products/:id', (req, res) => {
+    res.status(200).send({
+        success: true,
+        message: "Product deleted successfully",
+    });
 });
 
 
