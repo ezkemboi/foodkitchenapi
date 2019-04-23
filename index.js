cbb71f63d902c4a5f7b7be3e709a8becfb00c55e
me-x-mi <developerezrqn@gmail.com>
2019-04-23 12:31:48 +0300

    use sqlite3 for db

--- index.js
+++ index.js
@@ -1,11 +1,73 @@
 // External imports
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import cors from "cors";
+import Sequelize from "sequelize";
+import path from "path";
 
 // Local modules import
-import { Product, User } from "./models";
+// import { Product, User } from "./models";
+
+// import mongoose from 'mongoose';
+
+const dbPath = path.resolve(__dirname, "gennaskitchen.db");
+
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: dbPath
+});
+
+// Create tables
+const User = sequelize.define(
+  "user",
+  {
+    // attributes
+    firstName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    surname: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    },
+    username: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+  },
+  {
+    // options
+  }
+);
+
+const Product = sequelize.define(
+  "product",
+  {
+    // attributes
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    },
+    price: {
+      type: Sequelize.FLOAT,
+      allowNull: false
+    }
+  },
+  {
+    // options
+  }
+);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -17,14 +79,15 @@ app.use(
     extended: false
   })
 );
+app.use(cors());
 
 // Connect to MongoDB database
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/gennaskitchen",
-  {
-    useNewUrlParser: true
-  }
-);
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/gennaskitchen",
+//   {
+//     useNewUrlParser: true
+//   }
+// );
 
 // Set up router endpoint
 app.get("/", (req, res) => {
@@ -107,49 +170,43 @@ app.post("/login", async (req, res) => {
 });
 
 // Get all items in store
-app.get("/products", (req, res) => {
-  Product.find((err, products) => {
-    if (err) {
-      res.send(err);
-    }
-    if (!products || products.length < 1) {
-      res.status(404).send({
-        success: false,
-        message: "There are no products available at the moment"
-      });
-    }
-    res.status(200).send({
-      success: true,
-      message: "Products retrieved successfully",
-      data: products
+app.get("/products", async (req, res) => {
+  const allProducts = await Product.findAll();
+  if (!allProducts || allProducts.length < 1) {
+    res.status(404).send({
+      success: false,
+      message: "There are no products available at the moment"
     });
+  }
+
+  res.status(200).send({
+    success: true,
+    message: "Products retrieved successfully",
+    data: allProducts
   });
 });
 
 // Add food to the list of products
 app.post("/products", async (req, res) => {
   const { name, price } = req.body;
-  const findProduct = await Product.findOne({ name });
+  const findProduct = await Product.findOne({ where: { name: name } });
   if (findProduct) {
     // Let the user to know product already exist. Can update price or quantity in future
     res.status(409).send({
       success: false,
       message: "The product already exist"
     });
-  }
-  const newProduct = new Product();
-  newProduct.name = name;
-  newProduct.price = price;
-  newProduct.save(err => {
-    if (err) {
-      res.send(err);
-    }
+  } else {
+    const newProduct = await Product.create({
+      name: name,
+      price: price
+    });
     res.status(201).send({
       success: true,
       message: "Product added successfully",
       data: newProduct
     });
-  });
+  }
 });
 
 // Get a single item
@@ -206,6 +263,17 @@ app.delete("/products/:id", async (req, res) => {
     message: "Product deleted successfully"
   });
 });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch(err => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+sequelize.sync().done();
 
 app.listen(PORT, () => {
   console.log(`The server is running on ${PORT}`);
